84752ae0906b0d142ae8ce176246030bab1db975
vizeke <vizeke@gmail.com>
2018-08-27 15:08:27 -0300

    fix(ci): nome da imagem

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,6 @@ export RANCHER_STACK=$2
 export RANCHER_SERVICE=$3
 export RANCHER_START_FIRST=true
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $DOCKER_IMAGE
-
 #Atualiza a infra
 echo "Deploy no Rancher da imagem $DOCKER_IMAGE, env $RANCHER_ENV, stack $RANCHER_STACK, service $RANCHER_SERVICE."
 if [ ! -d "api-cloud-v2" ] ; then
@@ -18,4 +15,4 @@ cd api-cloud-v2
 npm install
 node ./client --ENVIRONMENT=$RANCHER_ENV \
     --STACK=$RANCHER_STACK --SERVICE=$RANCHER_SERVICE \
-    --IMAGE=$DOCKER_IMAGE --START_FIRST=$RANCHER_START_FIRST
+    --IMAGE=prodest/node-pushserver:$CIRCLE_TAG --START_FIRST=$RANCHER_START_FIRST
